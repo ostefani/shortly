3bfe03b126bfb5e089e164802cc98e3fe12e739b
ostefani <ostefani@protonmail.com>
2020-04-22 18:33:56 +0300

    Change dockerfile

--- server/webpack.config.server.js
+++ server/webpack.config.server.js
@@ -12,7 +12,7 @@ module.exports = {
     },
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, '../dist/api'),
     },
     // mute warning for the view.js
     stats: {
