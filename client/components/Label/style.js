173e913b418355e579337c4b7282490a217cfb35
ostefani <ostefani@protonmail.com>
2020-04-25 11:52:49 +0300

    Add docker file for client

--- client/components/Label/style.js
+++ client/components/Label/style.js
@@ -2,7 +2,8 @@ import css from 'styled-jsx/css';
 
 export default css`
     label {
-        color: var(--onbackground);
+        /*color: var(--onbackground);*/
+        color: green;
         font-size: var(--large);
         display: block;
         font-family: var(--main);
