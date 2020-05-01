02d1503900abc3dbacf9c33d396717e02254d71a
ostefani <ostefani@protonmail.com>
2020-05-01 17:40:35 +0300

    Allow any users to use shorten links

--- server/services/findURI.js
+++ server/services/findURI.js
@@ -3,9 +3,9 @@ import User from '../models/User';
 export default async (uri, name) => {
     try {
         const { _id, links } = await User.findOne(
-            { name },
             { links: { $elemMatch: { shortURI: uri } } },
         );
+        console.log('links: ', links);
         return { found: true, uri: links[0].longURI };
     }
     catch (e) {
